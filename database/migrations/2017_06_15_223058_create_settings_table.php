685f03dcc85f19b03b077d82c51f197fe9eb4f3c
kim109 <kim109@jiran.com>
2017-06-15 23:14:36 +0900

    * init

---
+++ database/migrations/2017_06_15_223058_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('state', ['ready', 'open', 'close'])->default('ready')->comment('진행 상태');
+            $table->unsignedSmallInteger('coin')->comment('개일별 지급 J-COIN');
+            $table->unsignedInteger('investment')->comment('실제 투자액');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
