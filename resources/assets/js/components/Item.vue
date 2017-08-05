c648a9f349bc564b3360a76d5efebfc98e00e38d
kim109 <kim109@jiran.com>
2017-08-05 22:12:30 +0900

    * 관리페이지 출석 이벤트 UI 작업중

--- resources/assets/js/components/Item.vue
+++ resources/assets/js/components/Item.vue
@@ -3,28 +3,40 @@
         <div class="panel panel-primary">
             <div class="panel-heading">{{this.index+1}}번 PT</div>
             <div class="panel-body form-horizontal">
-                <div class="form-group">
+                <div class="form-group form-group-sm">
                     <label class="col-sm-3 control-label">발표 제목</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control input-sm" placeholder="발표 제목" v-model="item.title">
+                        <input type="text" class="form-control" placeholder="발표 제목" v-model="item.title">
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">발표자 회사</label>
+                <div class="form-group form-group-sm">
+                    <label class="col-sm-3 control-label"><span class="hidden-sm">발표자 </span>회사</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control input-sm" placeholder="발표자 회사" v-model="item.company">
+                        <input type="text" class="form-control" placeholder="발표자 회사" v-model="item.company">
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">발표자 이름</label>
+                <div class="form-group form-group-sm">
+                    <label class="col-sm-3 control-label"><span class="hidden-sm">발표자 </span>이름</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control input-sm" placeholder="발표자 이름" v-model="item.speaker">
+                        <input type="text" class="form-control" placeholder="발표자 이름" v-model="item.speaker">
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">발표 내용 요약</label>
+                <div class="form-group form-group-sm">
+                    <label class="col-sm-3 control-label"><span class="hidden-sm">발표 </span>내용 요약</label>
                     <div class="col-sm-9">
-                        <textarea class="form-control input-sm" v-model="item.description"></textarea>
+                        <textarea class="form-control" rows="3" v-model="item.description"></textarea>
+                    </div>
+                </div>
+                <div class="form-group form-group-sm">
+                    <label class="col-sm-3 control-label"><span class="hidden-sm">출석 </span>이벤트</label>
+                    <div class="col-sm-9">
+                        <div class="input-group">
+                            <input type="number" min="1" max="300" class="form-control">
+                            <span class="input-group-btn">
+                                <button class="btn btn-sm btn-success"><span class="hidden-sm">이벤트 </span>시작</button>
+                                <button class="btn btn-sm btn-default">당첨자 확인</button>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
